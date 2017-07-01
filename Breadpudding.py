6a0b2eb8d1121cf35d4c1308bb8aa93a7f74212a
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-07-01 20:32:55 +0000

    Update Breadpudding.py

--- Breadpudding.py
+++ Breadpudding.py
@@ -1,26 +1,41 @@
+"""
+Module for the bot handler class that interfaces with the C.B. website API.
+"""
+
 import requests
 
-class Bot(object):
 
-    def __init__(self,nick="Bot",badge=None):
-        self.s = requests.session()
+class Bot(object):
+    """
+    Class for interfacing with C.B. API.
+    """
+    
+    def __init__(self, name, badge):
+        self.session = requests.session()
         self.nick = nick
         self.badge = badge
 
-    def send(self,message): # Sends a message.
-        r = self.s.post("http://www.breadpudding.us/api/chat/", data={"fn":"post","nick":self.nick,"badge":self.badge,"message":message})
-        return r.status_code
+    def send(self, message):
+        "Sends message via API"
+        
+        req = self.session.post("http://www.breadpudding.us/api/chat/", data=
+                                {"fn": "post", "nick": self.nick,
+                                 "badge": self.badge, "message": message})
+        return req.status_code
 
 
-    def setNick(self,nick): # Sets the nickname of the bot.
+    def set_nick(self, nick):
+        "Sets nickname for the bot via API"
         self.nick = nick
         return self.nick
 
-    def setBadge(self,badge): # Sets the badge of the bot.
+    def set_badge(self, badge):
+        "Sets guild badge via API"
         self.badge = badge
         return badge    
 
-Client = Bot("Nickname","Badge GUID")
+    
+client = Bot("Nickname","Badge GUID")
 
 # Bot stuff here.
 
