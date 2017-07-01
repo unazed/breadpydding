11a237f507e2c22887d7f52715c67098fb5737e7
CiscoRamonX <lemonv3rm@outlook.com>
2017-07-01 20:17:55 +0100

    Update Breadpudding.py

--- Breadpudding.py
+++ Breadpudding.py
@@ -7,17 +7,19 @@ class Bot(object):
         self.nick = "Bot"
         self.badge = "0"
 
-    def send(self,message):
+    def send(self,message): # Sends a message.
         r = self.s.post("http://www.breadpudding.us/api/chat/", data={"fn":"post","nick":self.nick,"message":message,"badge":self.badge})
         return r.status_code
 
 
-    def setNick(self,nick):
+    def setNick(self,nick): # Sets the nickname of the bot.
         self.nick = nick
         return self.nick
 
-    def setBadge(self,badge):
+    def setBadge(self,badge): # Sets the badge of the bot.
         self.badge = badge
         return badge    
 
 Client = Bot()
+
+# Bot stuff here.
